7c6a9e189f8c1f9c3a15693a7b71467fe7c5bfa3
FelipeToroR <felipe.toro.r01@mail.pucv.cl>
2023-06-15 14:51:27 -0400

    Sección Timeline sin bugs

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -17,60 +17,75 @@ use App\User;
 
 class UserProfileController extends Controller
 {
+    protected $user;
+
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user();
+            return $next($request);
+        });
+    }
+
+
     // perfil de usuario
     public function profile(){
-        return view('user_profile.user_profile');
+
+        return view('user_profile.user_profile')
+            ->with('user', $this->user);
     }
 
     public function about()
     {
+        $userProfile = $this->user->userProfile;
 
-        $user = Auth::user();
-        $userProfile = $user->userProfile;
-
-
-        return view('user_profile.about',compact('user', 'userProfile'));
+        return view('user_profile.about',compact('userProfile'))
+            ->with('user' , $this->user);
     }
 
     public function timeline()
     {
-        $user = Auth::user();
-
-        // Recupera los experimentos activos de los usuarios
+        // Recupera los experimentos activos de los usuarios (ordenados desde el mas reciente)
         $game_instances_user_list = Experiment::leftjoin('user_experiments', 'experiments.id', '=', 'user_experiments.experiment_id')
             ->where('experiments.status', 1)
-            ->where('user_experiments.user_id', $user->id);
+            ->where('user_experiments.user_id', $this->user->id)
+            ->orderBy('experiments.updated_at', 'asc')
+            ->get();
+
 
         $game_instances_list_count = $game_instances_user_list->count();
 
         if ($game_instances_list_count > 1 || $game_instances_list_count == 0) {
             // Si tienes más de un experimento o ninguno, muestra el listado.
             return view('user_profile.timeline')
-                ->with('game_instances_user_list', $game_instances_user_list->get());
+                ->with('game_instances_user_list', $game_instances_user_list)
+                ->with('user', $this->user);
         } else {
             return view('user_profile.timeline')
-            ->with('game_instances_user_list', $game_instances_user_list->get());
+                ->with('game_instances_user_list', $game_instances_user_list)
+                ->with('user', $this->user);
         }
     }
 
     public function medallas()
     {
-        return view('user_profile.medallas');
+        
+        return view('user_profile.medallas')
+        ->with('user', $this->user);
     }
 
     public function update(Request $request)
     {
-        $user = Auth::user();
-        $userProfile = $user->userProfile;
+        $userProfile = $this->user->userProfile;
 
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->save();
+        $this->user->name = $request->input('name');
+        $this->user->email = $request->input('email');
+        $this->user->save();
 
          // Crear el perfil de usuario si no existe
          if (!$userProfile) {
             $userProfile = new UserProfile();
-            $userProfile->user_id = $user->id;
+            $userProfile->user_id = $this->user->id;
         }
 
         // Actualizar los campos en el modelo UserProfile
--- resources/views/user_profile/user_profile.blade.php
+++ resources/views/user_profile/user_profile.blade.php
@@ -12,11 +12,11 @@
       <div class="profile">
         <div class="profile-avatar"> 
           <img src="{{url('assets/img/users.png')}}" alt="" class="profile-img">
-          <div class="profile-name">Admin</div>
+          <div class="profile-name">{{ $user->name }}</div>
         </div>
         <img src="https://images.unsplash.com/photo-1508247967583-7d982ea01526?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="" class="profile-cover">
         <div class="profile-menu">
-          <a class="profile-menu-link" style= "margin-left: 7%;" href="{{ route('user_profile.about')    }}">About</a>
+          <a class="profile-menu-link" href="{{ route('user_profile.about')    }}">About</a>
           <a class="profile-menu-link" href="{{ route('user_profile.timeline') }}">Timeline</a>
           <a class="profile-menu-link" href="{{ route('user_profile.medallas') }}">Medallas</a>
         </div>
